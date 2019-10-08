134112e061cbc128839af99bc95b6a9d55e5d960
Navneet Lal Gupta <navneetlalg@gmail.com>
2019-10-08 11:13:53 +0530

    initial commit

---
+++ src/main/java/com/navneetlalg/java_problems/App.java
@@ -0,0 +1,23 @@
+package com.navneetlalg.java_problems;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public boolean isUpperCase(String value)
+    {
+      return true;
+    }
+
+    public int getRandomValue()
+    {
+      return 0;
+    }
+}
